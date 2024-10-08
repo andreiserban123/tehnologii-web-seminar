601745d8332c10900044ab0dea62f542024f02f6
Your Name <andyspeed2003@gmail.com>
2024-10-08 17:29:38 +0300

    sem2

---
+++ seminar2/let_const.js
@@ -0,0 +1,10 @@
+// let test = 3;
+// test = "3";
+
+// console.log(typeof test);
+
+const obj = {};
+obj.nume = "Andrei";
+obj.nume = "Matei";
+console.log(obj.nume);
+obj = { ...obj, name: "Mihai" };
